49d7ff0bf1562ddf37bbeac3e296fd671e061993
Oguzhan Cetinkaya <cetinkaya-oguzhan@hotmail.com>
2025-10-16 21:05:41 +0200

    implement responsive layout and add logo loop component for skills section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,37 @@
 import ShinyText from '../components/ShinyText/ShinyText';
 import "../css/Home.css"
+import profilbild from '../assets/profil/profilbild.jpg';
 
 export default function Home(){
     return (
-    <div id='intro-container'>
-        <ShinyText text="Willkommen auf meiner Portfolio-Seite!
-        Mein Name ist Oguzhan Cetinkaya. Ich bin im Jahr 1992 in der Türkei geboren,
-        wo ich an der Universität Rechtwissenschaft studiert habe. Ich bin Anwalt.
-        Mein Interesse liegt mehr auf Mathematik, Technologie, deshalb habe ich mich entschieden,
-        Software-Entwickler zu sein. Dafür mache ich eine Berufslehre als Applikationsentwickler an der Benedict Schule.
-        Seit 2021 lebe ich in der Schweiz. Ich fühle mich hier sehr wohl." speed={3} />
-    </div>
+        <div className="home-page">
+            <div className="home-row">
+                <img src={profilbild} alt="Profil" className="profile-img" />
+                <div id='intro-container'>
+                    <ShinyText text="Willkommen auf meiner Portfolio-Seite!
+                    Mein Name ist Oguzhan Cetinkaya. Ich bin im Jahr 1992 in der Türkei geboren,
+                    wo ich an der Universität Rechtwissenschaft studiert habe. Ich bin Anwalt.
+                    Mein Interesse liegt mehr auf Mathematik, Technologie, deshalb habe ich mich entschieden,
+                    Software-Entwickler zu sein. Dafür mache ich eine Berufslehre als Applikationsentwickler an der Benedict Schule.
+                    Seit 2021 lebe ich in der Schweiz. Ich fühle mich hier sehr wohl." speed={3} />
+                </div>
+            </div>
+            <div className="contact-row">
+                <fieldset className="kontaktformular">
+                    <legend>Schnellkontakt-Formular</legend>
+                    <form name="contact" method="POST" data-netlify="true">
+                        <input type="hidden" name="form-name" value="contact" />
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" name="name" id="name" placeholder="Dein Name" required />
+                        <label htmlFor="email">E-Mail:</label>
+                        <input type="email" name="email" id="email" placeholder="deine@email.com" required />
+                        <label htmlFor="message">Nachricht:</label>
+                        <textarea name="message" id="message" placeholder="Deine Nachricht" required></textarea>
+                        <button type="submit">Senden</button>
+                    </form>
+                    <br /><div id="result"></div>
+                </fieldset>
+            </div>
+        </div>
     );
 }
--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,3 +1,124 @@
+import LogoLoop from '../components/LogoLoop/LogoLoop';
+import "../css/Skills.css"
+import cssLogo from '../assets/imageLogos/css.png';
+import jsLogo from '../assets/imageLogos/javascript.png';
+import tsLogo from '../assets/imageLogos/typescript.png';
+import pythonLogo from '../assets/imageLogos/python.png';
+import reactLogo from '../assets/imageLogos/react.png';
+import nodejsLogo from '../assets/imageLogos/nodejs.png';
+import mongodbLogo from '../assets/imageLogos/mongodb.png';
+import mysqlLogo from '../assets/imageLogos/mysql.png';
+import supaBaseLogo from '../assets/imageLogos/supabase.png';
+import dockerLogo from '../assets/imageLogos/docker.png';
+import vscode from '../assets/imageLogos/vscode.png';
+import jetbrains from '../assets/imageLogos/jetbrains.png';
+
+
+const languageLogos = [
+    { src: cssLogo, alt: "CSS"},
+    { src: jsLogo, alt: "JavaScript" },
+    { src: tsLogo, alt: "TypeScript"},
+    { src: pythonLogo, alt: "Python"},
+];
+
+const frameworkLogos = [
+    { src: reactLogo, alt: "React",  },
+    { src: nodejsLogo, alt: "Node.js" },
+];
+
+const databaseLogos = [
+    { src: mongodbLogo, alt: "MongoDB"},
+    { src: mysqlLogo, alt: "MySQL"},
+    { src: supaBaseLogo, alt: "SupaBase"},
+];
+
+const toolLogos = [
+    { src: vscode, alt: "VS Code"},
+    { src: dockerLogo, alt: "Docker"},
+    { src: jetbrains, alt: "JetBrains"},
+];
+
 export default function Skills() {
-    return
+    return(
+        <div className="skills-page">
+            <div>
+                <h1>Meine technischen Fähigkeiten</h1>
+                <p>Hier sind einige der Technologien und Werkzeuge, mit denen ich Erfahrung habe:</p>
+            </div>
+
+            <div>
+                <h2>Sprachen & Styles(JavaScript, TypeScript, Python, CSS)</h2>
+            </div>
+
+            <div style={{ height: '200px', position: 'relative', overflow: 'hidden'}}>
+                <LogoLoop
+                    logos={languageLogos}
+                    speed={50}
+                    direction="left"
+                    logoHeight={50}
+                    gap={100}
+                    pauseOnHover
+                    scaleOnHover
+                    fadeOut
+                    fadeOutColor="#ffffff"
+                    ariaLabel="Sprachen und Styles Logos"
+                />
+            </div>
+
+            <div>
+                <h2>Frameworks & Runtimes (React, Node.js)</h2>
+            </div>
+            <div style={{ height: '200px', position: 'relative', overflow: 'hidden'}}>
+                <LogoLoop
+                    logos={frameworkLogos}
+                    speed={50}
+                    direction="right"
+                    logoHeight={45}
+                    gap={100}
+                    pauseOnHover
+                    scaleOnHover
+                    fadeOut
+                    fadeOutColor="#ffffff"
+                    ariaLabel="Runtimes und Bibliotheken Logos"
+                />
+            </div>
+
+            <div>
+                <h2>Datenbanken (MongoDB, MySQL, Supabase)</h2>
+            </div>
+            <div style={{ height: '200px', position: 'relative', overflow: 'hidden'}}>
+                <LogoLoop
+                    logos={databaseLogos}
+                    speed={75}
+                    direction="left"
+                    logoHeight={40}
+                    gap={100}
+                    pauseOnHover
+                    scaleOnHover
+                    fadeOut
+                    fadeOutColor="#ffffff"
+                    ariaLabel="Datenbanken Logos"
+                />
+            </div>
+
+            <div>
+                <h2>Tools (VS Code, Docker, JetBrains)</h2>
+            </div>
+            <div style={{ height: '200px', position: 'relative', overflow: 'hidden'}}>
+                <LogoLoop
+                    logos={toolLogos}
+                    speed={75}
+                    direction="right"
+                    logoHeight={35}
+                    gap={500}
+                    pauseOnHover
+                    scaleOnHover
+                    fadeOut
+                    fadeOutColor="#ffffff"
+                    ariaLabel="Tools Logos"
+                />
+            </div>
+
+        </div>
+    )
 }
